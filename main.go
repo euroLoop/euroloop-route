bb93ea7105c4d5bf170c29d9298163d6cb64ea91
Joakim <joakim.fu@gmail.com>
2017-09-23 17:45:45 +0200

    CORS headers

--- main.go
+++ main.go
@@ -61,6 +61,7 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 	var data RouteData
 	w.Header().Add("Access-Control-Allow-Origin", "*")
 	w.Header().Add("Content-Type", "application/json")
+	w.Header().Add("Access-Control-Allow-Headers", "Content-Type")
 	body, _ := ioutil.ReadAll(r.Body)
 	fmt.Println(string(body))
 	_ = json.Unmarshal(body, &data)
